7790773be0b3b4de929489fb8e4fed40ea18f360
Anatolie Lupacescu <anatollupacescu@gmail.com>
2018-05-10 18:07:38 +0200

    add start bash script

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+TUMBLR_CLIENT_KEY=<key> \
+TUMBLR_CLIENT_SECRET=<secret> \
+TUMBLR_TOKEN=<token> \
+TUMBLR_TOKEN_SECRET=<token secret> \
+java -jar spring/target/spring-0.0.1-SNAPSHOT-fat.jar
